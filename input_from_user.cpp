ffa54d41a2c468f321de063b36ecedf0419c0ffd
gayatri810 <apotikargr08@gmail.com>
2024-08-06 00:02:16 +0530

    Add files via upload

---
+++ input_from_user.cpp
@@ -0,0 +1,12 @@
+//Gayatri apotikar
+//23070123055
+#include<iostream>
+using namespace std;
+int main()
+{
+   int num;
+   cout<<"Enter a number: ";
+   cin>>num;
+   cout<<"The number enter by you is: "<<num;
+   return 0;
+}
